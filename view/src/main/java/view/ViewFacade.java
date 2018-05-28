fa7d5c3771eb0b6b3d49137e9fefe6283c887e0c
Matthieu07 <33153593+Matthieu07@users.noreply.github.com>
2018-05-28 14:13:09 +0200

    Add files via upload

---
+++ view/src/main/java/view/ViewFacade.java
@@ -0,0 +1,29 @@
+package view;
+
+import javax.swing.JOptionPane;
+
+/**
+ * <h1>The Class ViewFacade provides a facade of the View component.</h1>
+ *
+ * @author Jean-Aymeric DIET jadiet@cesi.fr
+ * @version 1.0
+ */
+public class ViewFacade implements IView {
+
+    /**
+     * Instantiates a new view facade.
+     */
+    public ViewFacade() {
+        super();
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see view.IView#displayMessage(java.lang.String)
+     */
+    @Override
+    public final void displayMessage(final String message) {
+        JOptionPane.showMessageDialog(null, message);
+    }
+
+}
---
+++ view/src/main/java/view/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Provides all classes for the view component.
+ *
+ * @author Jean-Aymeric DIET jadiet@cesi.fr
+ * @version 1.0
+ */
+package view;
