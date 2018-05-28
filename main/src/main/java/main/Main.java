e6c08dbfb26878b951b2850632ccaf5340419961
Matthieu07 <33153593+Matthieu07@users.noreply.github.com>
2018-05-28 16:05:07 +0200

    salut

--- main/src/main/java/main/Main.java
+++ main/src/main/java/main/Main.java
@@ -7,7 +7,7 @@ import model.ModelFacade;
 import view.ViewFacade;
 
 /**
- * <h1>The Class test Main.</h1>
+ * <h1>The Class bite Main.</h1>
  *
  * @author Jean-Aymeric DIET jadiet@cesi.fr
  * @version 1.0
