be443f4df79326465d57fa081327b9e9a4c1cc66
Azrog <gromairebaptiste@gmail.com>
2018-05-29 09:11:42 +0200

    jfram viewfacade

--- view/src/main/java/view/ViewFacade.java
+++ view/src/main/java/view/ViewFacade.java
@@ -2,6 +2,12 @@ package view;
 
 import javax.swing.JOptionPane;
 
+import java.awt.Color;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+
 /**
  * <h1>The Class ViewFacade provides a facade of the View component.</h1>
  *
@@ -25,5 +31,15 @@ public class ViewFacade implements IView {
     public final void displayMessage(final String message) {
         JOptionPane.showMessageDialog(null, message);
     }
-
+    
+    public void printMap(String level) {
+    	JFrame window = new JFrame();
+        window.setSize(640, 416);
+        window.setTitle("Lorann");
+        window.setLocationRelativeTo(null);
+        window.setResizable(false);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel pan = new JPanel();
+        pan.setBackground(Color.BLACK);
+    }
 }
